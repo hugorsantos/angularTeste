f4a456567b0739c9e7edd542989e58cae57e47a1
hugorsantos <hugorsantos@gmail.com>
2016-12-26 18:44:35 -0200

    Adicao dos arquivos do projeto angularTeste

---
+++ src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('angularTeste')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
---
+++ src/app/index.module.js
@@ -0,0 +1,18 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('angularTeste',
+        ['ngAnimate',
+          'ngCookies',
+          'ngTouch',
+          'ngSanitize',
+          'ngMessages',
+          'ngAria',
+          'ngResource',
+          'ngRoute',
+          'ui.bootstrap',
+          'toastr'
+        ]);
+
+})();
